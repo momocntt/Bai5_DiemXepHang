af0868ff99871400368560ff48fd005514232cd6
momocntt <nguyenthihoaimo1207@gmail.com>
2024-11-04 16:44:23 +0700

    Add project files.

---
+++ Bai5_DiemXepHang.cpp
@@ -0,0 +1,22 @@
+// Bai5_DiemXepHang
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "CHUONG TRINH XEP HANG TU DIEM\n";
+    double diem;
+    cout << "Nhap diem: "; cin >> diem;
+    if (diem < 5) {
+        cout << "Yeu";
+    } else if (diem <= 6) {
+        cout << "TB";
+    } else if (diem < 8) {
+        cout << "Kha";
+    }
+    else {
+        cout << "Gioi";
+    }
+return 0;
+    }
